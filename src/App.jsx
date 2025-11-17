0ab1fda05b6b058e67c60b681c15b7484c34420d
Reise Young <reise@Reises-MacBook-Pro.local>
2025-11-17 14:30:14 -0800

    Fix incorrect component import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Skills from "./components/Skills.jsx";
+import Projects from "./components/Projects.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
+
 
 export default function App() {
   return (
